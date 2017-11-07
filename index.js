61b2376e01b73b99bb6e0ac472c3e9b99f74208a
PJ Frias <peejfrias@gmail.com>
2017-11-07 02:55:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
   const matchingDrivers = drivers.filter(function(name) {return name.toUpperCase() === string.toUpperCase()})
-  console.log(matchingDrivers)
+  ///console.log(matchingDrivers)
   return matchingDrivers
 }
 
