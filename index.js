ff61e86d54b5fdcc5f6fad0a63d2731460f0b63c
PJ Frias <peejfrias@gmail.com>
2017-11-07 02:26:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
-  const filterDrivers = drivers.filter(function(name) {return name.toUpperCase() === string.toUpperCase()})
-  console.log(filterDrivers)
-  return filterDrivers
+  const matchingDrivers = drivers.filter(function(name) {return name.toUpperCase() === string.toUpperCase()})
+  console.log(matchingDrivers)
+  return matchingDrivers
 }
 
 function fuzzyMatch(drivers, string) {
-  
+
 }
 
 function matchName(drivers, string) {
-  
+  const matchingDrivers = drivers.filter(function(name) {return name.toUpperCase() === string.toUpperCase()})
+  console.log(matchingDrivers)
+  return matchingDrivers
 }
