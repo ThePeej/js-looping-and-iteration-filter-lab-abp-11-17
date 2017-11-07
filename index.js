e0fbb7f662894e2f8b711cf07c170d4b15cff68f
PJ Frias <peejfrias@gmail.com>
2017-11-07 02:57:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function matchName(drivers, string) {
       driverMatch.push(user)
     }
   }
-  console.log(driverMatch)
-  debugger;
+  ///console.log(driverMatch)
+  ///debugger;
   return driverMatch
 }
